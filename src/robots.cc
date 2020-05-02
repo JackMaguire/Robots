bf205c2da5d28dfd18be6ad7f09e9ef705eea650
Jack Maguire <jackmaguire1444@gmail.com>
2020-05-02 15:10:47 -0400

    more progress

--- src/robots.cc
+++ src/robots.cc
@@ -2,12 +2,15 @@
 
 #include <array>
 #include <vector>
+#include <set>
+
 #include <cstdlib> //rand()
 #include <random>
 #include <iostream>
 #include <algorithm>
 
 using uint = unsigned int;
+using luint = long unsigned int;
 
 constexpr uint WIDTH = 32;
 constexpr uint HEIGHT = 24;
@@ -18,10 +21,22 @@ enum class Occupant
 {
  EMPTY = 0,
  ROBOT,
- HUMAN
+ HUMAN,
+ FIRE
 };
 
+enum class MoveResult
+{
+ CONTINUE,
+ YOU_LOSE,
+ YOU_WIN_ROUND,
+ YOU_WIN_GAME
+};
+
+
 struct Position {
+
+  // auto operator<=>(const Position&) const = default;
 
   bool operator==( Position const & o ) const {
     return x == o.x && y == o.y;
@@ -50,7 +65,7 @@ uint random_y(){
 
 class Board {
   Board(){
-    init();
+    init( 1 );
   }
 
   void clear_board(){
@@ -61,7 +76,7 @@ class Board {
     }
   }
 
-  void init(){
+  void init( uint const round ){
     //RESET
     clear_board();
 
@@ -70,7 +85,7 @@ class Board {
     cell( human_position_ ) = Occupant::HUMAN;
     
     //ROBOTS
-    robot_positions_.resize( nrobots_per_round( round_ ) );
+    robot_positions_.resize( nrobots_per_round( round ) );
     if( robot_positions_.size() < 100 ){
       //Use different algorithm if the robot count is << the number of cells
       for( Position & robot : robot_positions_ ){
@@ -113,17 +128,76 @@ class Board {
     return cells_[ p.x ][ p.y ];
   }
 
+  MoveResult
+  move_robots_1_step(){
+    //Clear robots from map
+    for( uint w = 0; w < WIDTH; ++w ){
+      for( uint h = 0; h < HEIGHT; ++h ){
+	if( cells_[ w ][ h ] == Occupant::ROBOT ){
+	  cells_[ w ][ h ] = Occupant::EMPTY;
+	}
+      }
+    }
+
+    std::set< uint > robots_to_delete;
+ 
+    //keep temporary track of robots just in case they clash
+    //elements are indices in robot_positions_
+    std::array< std::array< uint , WIDTH >, HEIGHT > robot_indices;
+
+    for( uint r = 0; r < robot_positions_.size(); ++r ){
+      Position & pos = robot_positions_[ r ];
+
+      if( human_position_.x < pos.x ) pos.x -= 1;
+      else if( human_position_.x > pos.x ) pos.x += 1;
+
+      if( human_position_.y < pos.y ) pos.y -= 1;
+      else if( human_position_.y > pos.y ) pos.y += 1;
+
+      switch( cell( pos ) ){
+      case Occupant::EMPTY:
+	robot_indices[ pos.x ][ pos.y ] = r;
+	cell( pos ) = Occupant::ROBOT;
+	break;
+      case Occupant::ROBOT:
+	{
+	  uint const other_robot_ind = robot_indices[ pos.x ][ pos.y ];
+	  robots_to_delete.insert( other_robot_ind );
+	}
+	robots_to_delete.insert( r );
+	cell( pos ) = Occupant::FIRE;
+	break;
+      case Occupant::HUMAN:
+	return MoveResult::YOU_LOSE;
+	//break;
+      case Occupant::FIRE:
+	robots_to_delete.insert( r );
+	break;
+      }
+    }// for uint r
+
+    for( auto iter = robots_to_delete.rbegin(), end = robots_to_delete.rend();
+	 iter != end; ++iter ){
+      robot_positions_.erase( robot_positions_.begin() + (*iter) );
+    }
+
+    return MoveResult::CONTINUE;
+  } //move_robots_one_step
+
+
 private:
   std::array< std::array< Occupant, WIDTH >, HEIGHT > cells_;
+  //TODO hold pointers to robots?
 
   Position human_position_;
   std::vector< Position > robot_positions_;
 
-  uint round_ = 1;
 };
 
 class RobotsGame {
-  
+
+  uint round_ = 1;
+  luint score_ = 0;    
 };
 
 //int main(){};

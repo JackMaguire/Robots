df6e8385926c1f22cc7e9b247495ad4a6a2b502d
Jack Maguire <jackmaguire1444@gmail.com>
2020-05-03 11:09:08 -0400

    let's use arcade

---
+++ robots.Tkinter.py
@@ -0,0 +1,59 @@
+from Tkinter import *
+ 
+#https://subscription.packtpub.com/book/web_development/9781788622301/1/ch01lvl1sec20/handling-mouse-and-keyboard-events
+class App(Tk): 
+    def __init__(self): 
+        pass
+        super().__init__() 
+        '''
+        frame = tk.Frame(self, bg="green", height=100, width=100) 
+        frame.bind("<Button-1>", self.print_event) 
+        frame.bind("<Double-Button-1>", self.print_event) 
+        frame.bind("<ButtonRelease-1>", self.print_event) 
+        frame.bind("<B1-Motion>", self.print_event) 
+        frame.bind("<Enter>", self.print_event) 
+        frame.bind("<Leave>", self.print_event) 
+        frame.pack(padx=50, pady=50) 
+        '''
+    '''
+    def print_event(self, event): 
+    position = "(x={}, y={})".format(event.x, event.y) 
+    print(event.type, "event", position) 
+    '''
+ 
+master = Tk()
+
+width = 45
+height = 30
+
+box_size = 20
+canvas_width = width * box_size
+canvas_height = height * box_size
+
+color="#CCCCCC"
+
+w = Canvas(master, 
+           width=canvas_width,
+           height=canvas_height,
+           borderwidth=0, highlightthickness=0)
+w.pack()
+
+def print_background():
+    for i in range( 0, width / 2 ):
+        for k in range( 0, height ):
+            j = i * 2 +( k % 2 )
+            x1 = j * box_size
+            y1 = k * box_size
+            x2 = x1 + box_size
+            y2 = y1 + box_size
+            w.create_rectangle( x1, y1, x2, y2, fill=color, outline=color)
+
+print_background()
+
+#https://stackoverflow.com/questions/29158220/tkinter-understanding-mainloop
+mainloop()
+#master.update_idletasks()
+#master.update()
+
+#while True:
+#    pass
--- robots.py
+++ robots.py
@@ -1,30 +1,7 @@
-from Tkinter import *
+import arcade
  
-#https://subscription.packtpub.com/book/web_development/9781788622301/1/ch01lvl1sec20/handling-mouse-and-keyboard-events
-class App(Tk): 
-    def __init__(self): 
-        pass
-        super().__init__() 
-        '''
-        frame = tk.Frame(self, bg="green", height=100, width=100) 
-        frame.bind("<Button-1>", self.print_event) 
-        frame.bind("<Double-Button-1>", self.print_event) 
-        frame.bind("<ButtonRelease-1>", self.print_event) 
-        frame.bind("<B1-Motion>", self.print_event) 
-        frame.bind("<Enter>", self.print_event) 
-        frame.bind("<Leave>", self.print_event) 
-        frame.pack(padx=50, pady=50) 
-        '''
-    '''
-    def print_event(self, event): 
-    position = "(x={}, y={})".format(event.x, event.y) 
-    print(event.type, "event", position) 
-    '''
- 
-master = Tk()
-
-width = 48
-height = 32
+width = 45
+height = 30
 
 box_size = 20
 canvas_width = width * box_size
@@ -32,28 +9,47 @@ canvas_height = height * box_size
 
 color="#CCCCCC"
 
-w = Canvas(master, 
-           width=canvas_width,
-           height=canvas_height,
-           borderwidth=0, highlightthickness=0)
-w.pack()
+def draw_background():
+    """
+    This function draws the background. Specifically, the sky and ground.
+    """
+    # Draw the sky in the top two-thirds
+    arcade.draw_lrtb_rectangle_filled(0,
+                                      canvas_width,
+                                      canvas_height,
+                                      0,
+                                      arcade.color.WHITE)
 
-def print_background():
-    for i in range( 0, width / 2 ):
+    for i in range( 0, int(width / 2) + 1 ):
         for k in range( 0, height ):
             j = i * 2 +( k % 2 )
             x1 = j * box_size
             y1 = k * box_size
             x2 = x1 + box_size
             y2 = y1 + box_size
-            w.create_rectangle( x1, y1, x2, y2, fill=color, outline=color)
+            arcade.draw_lrtb_rectangle_filled( x1, x2, y2, y1, arcade.color.GRAY );
 
-print_background()
 
-#https://stackoverflow.com/questions/29158220/tkinter-understanding-mainloop
-#mainloop()
-master.update_idletasks()
-master.update()
+def main():
+    # Open the window
+    arcade.open_window(canvas_width, canvas_height, "robots")
 
-#while True:
-#    pass
+    # Start the render process. This must be done before any drawing commands.
+    arcade.start_render()
+
+    # Call our drawing functions.
+    draw_background()
+    #draw_pine_tree(50, 250)
+    #draw_pine_tree(350, 320)
+    #draw_bird(70, 500)
+    #draw_bird(470, 550)
+
+    # Finish the render.
+    # Nothing will be drawn without this.
+    # Must happen after all draw commands
+    arcade.finish_render()
+
+    # Keep the window up until someone closes it.
+    arcade.run()
+
+main()

8be84d44b05b5f1998b421ebb5420c8afedf1d09
Docker User <jackmaguire1444@gmail.com>
2020-05-09 18:35:55 +0000

    bindings compile for the game

--- src/model_for_nn.cc
+++ src/model_for_nn.cc
@@ -1,22 +1,38 @@
 #include "robots.cc"
 
 #include <boost/python.hpp>
-#include <boost/python/numpy.hpp>
+//#include <boost/python/numpy.hpp>
 
 using namespace boost;
 using namespace boost::python;
-using namespace boost::python::numpy;
+//using namespace boost::python::numpy;
 
 namespace p = boost::python;
-namespace np = boost::python::numpy;
+//namespace np = boost::python::numpy;
+
+namespace {
+  class DummyVisualizer {
+  public:
+    static void show( Board const & ){}
+  };
+}
 
 BOOST_PYTHON_MODULE( model_for_nn )
 {
   using namespace boost::python;
   Py_Initialize();
-  np::initialize();
+  //np::initialize();
 
-  class_<Board>("FilePos")
-    .def("get_stringified_representation", &Base::get_stringified_representation )
-    .def("load_from_stringified_representation", &Base::load_from_stringified_representation );
+  class_<Board>("Board")
+    .def("get_stringified_representation", &Board::get_stringified_representation )
+    .def("load_from_stringified_representation", &Board::load_from_stringified_representation );
+
+  using Game = RobotsGame< DummyVisualizer >;
+  class_<Game>("Game")
+    .def( "cascade_fast", &Game::cascade< false > )
+    .def( "move_human", &Game::move_human )
+    .def( "teleport", &Game::teleport )
+    .def( "n_safe_teleports_remaining", &Game::n_safe_teleports_remaining )
+    .def( "round", &Game::round )
+    .def( "load_from_stringified_representation", &Board::load_from_stringified_representation );
 }

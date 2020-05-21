38454ab74440a76050fefa43a1ce87114d239845
Jack Maguire <jackmaguire1444@gmail.com>
2020-05-21 14:16:57 -0400

    text

--- src/wt/sidebar.hh
+++ src/wt/sidebar.hh
@@ -2,14 +2,15 @@
 
 //#include <Wt/WContainerWidget.h>
 #include <Wt/WTable.h>
-#include <Wt/WLineEdit.h>
+#include <Wt/WText.h>
 
 //#include "board_widget.hh"
 class ScoreWidget : public Wt::WTable {
 public:
   ScoreWidget(){
     setHeaderCount(0);
-    elementAt(0, 0)->addNew< Wt::WLineEdit >( "Score" );
+    elementAt(0, 0)->addNew< Wt::WText >( "Score:" );
+    elementAt(1, 0)->addNew< Wt::WText >( "0" );
   }
 
 private:

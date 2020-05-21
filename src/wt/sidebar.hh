ef80e8973b1461bff9a2c8f6f742de0c25d856d3
Jack Maguire <jackmaguire1444@gmail.com>
2020-05-21 14:23:10 -0400

    update

--- src/wt/sidebar.hh
+++ src/wt/sidebar.hh
@@ -2,17 +2,25 @@
 
 //#include <Wt/WContainerWidget.h>
 #include <Wt/WTable.h>
-#include <Wt/WText.h>
+#include <Wt/WLineEdit.h>
+
+#include <string>
 
 //#include "board_widget.hh"
 class ScoreWidget : public Wt::WTable {
 public:
   ScoreWidget(){
     setHeaderCount(0);
-    elementAt(0, 0)->addNew< Wt::WText >( "Score:" );
-    elementAt(1, 0)->addNew< Wt::WText >( "0" );
+
+    Widget * score_element = elementAt(0, 0)->addNew< Wt::WLineEdit >( "Score: 0" );
+    score_ = dynamic_cast< Wt::WLineEdit * >( score_element );
+    score_->setReadOnly( true );
+  }
+
+  void update_with_new_score( long unsigned int const score ){
+    score_->setText( "Score: " + std::to_string( score ) );
   }
 
 private:
-  
+  Wt::WLineEdit * score_;
 };

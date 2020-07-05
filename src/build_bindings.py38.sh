d00f0352f0b313ab9dea81c70297cf4ae7190b0a
JackMaguire <jackmaguire1444@gmail.com>
2020-07-05 18:10:54 +0000

    build_bindings.py38.sh

--- src/build_bindings.py38.sh
+++ src/build_bindings.py38.sh
@@ -15,6 +15,6 @@ debug=""
 
 $CXX -c -fPIC $name.cc -o ${name}.o $WARN -O3 -std=c++17 -I/usr/include/python3.8 $debug
 
-#$CXX -L /usr/lib/x86_64-linux-gnu $WARN -fPIC -shared -Wl,${thingy},${name}.so -O3 -std=c++17 -o ${name}.cpython-36m-x86_64-linux-gnu.so ${name}.o -I/usr/include/python3.8m -lboost_system -lboost_python38 -lpython3.7m -lboost_numpy3 $debug
+#$CXX -L /usr/lib/x86_64-linux-gnu $WARN -fPIC -shared -Wl,${thingy},${name}.so -O3 -std=c++17 -o ${name}.cpython-38-x86_64-linux-gnu.so ${name}.o -I/usr/include/python3.8m -lboost_system -lboost_python38 -lpython3.7m -lboost_numpy3 $debug
 
-$CXX -L /usr/lib/x86_64-linux-gnu $WARN -fPIC -shared -Wl,-soname,${name}.so -O3 -std=c++11 -o ${name}.cpython-36m-x86_64-linux-gnu.so ${name}.o -I/usr/include/python3.8 -lboost_system -lboost_python38 -lpython3.8 -lboost_numpy38 $debug
+$CXX -L /usr/lib/x86_64-linux-gnu $WARN -fPIC -shared -Wl,-soname,${name}.so -O3 -std=c++11 -o ${name}.cpython-38-x86_64-linux-gnu.so ${name}.o -I/usr/include/python3.8 -lboost_system -lboost_python38 -lpython3.8 -lboost_numpy38 $debug

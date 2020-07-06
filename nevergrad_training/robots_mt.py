ff79edecee4cf8f5f6077ce02b0a854d732a9ae0
JackMaguire <jackmaguire1444@gmail.com>
2020-07-06 13:59:07 +0000

    putting multithreading in its own corner

--- nevergrad_training/robots_mt.py
+++ nevergrad_training/robots_mt.py
@@ -32,9 +32,7 @@ import nevergrad as ng
 #args = parser.parse_args()
 
 #Board Input
-
-def create_model():
-
+def create_model2():
     view_distance = 4
     n_board_states = 5 # OOB, empty, human, fire, robot (TODO reorder to match enum)
     in1_dim = 1 + (2*view_distance)
@@ -60,7 +58,7 @@ def create_model():
     layer = Conv2D( filters=10, kernel_size=(1,1), padding='valid', activation='relu' )( layer )
     layer = Conv2D( filters=5, kernel_size=(1,1), padding='valid', activation='relu' )( layer )
 
-    
+
     #This should be of shape( 3, 3, n_board_states + 5 )
     layer = tensorflow.keras.layers.concatenate( [boardconv1,layer], name="merge", axis=-1 )
 
@@ -76,7 +74,7 @@ def create_model():
 
     input3 = Input(shape=(1,), name="in3", dtype="float32" )
     layer = tensorflow.keras.layers.concatenate( [input3,layer], name="merge_teleport", axis=-1 )
-    
+
     layer = Dense( units=25, activation='relu' )( layer )
     #layer = Dense( units=100, activation='relu' )( layer )
 
@@ -95,6 +93,12 @@ def create_model():
     model.set_weights(initial_weights)
 
     return model
+
+
+def create_model():
+    gra = tf.Graph()
+    with gra.as_default():
+        return create_model2()
 
 def parameterization_for_model( model ):
 
@@ -148,69 +152,26 @@ def run_single_simulation( model ):
     
     return game.score()
 
-def dump_model( weights, filename ):
-    model = create_model()
-    model.set_weights([tf.convert_to_tensor(arg, dtype=tf.float32) for arg in weights.args])
-    model.save( filename )
+def dump_model( weights_param, filename ):
+    gra = tf.Graph()
+    with gra.as_default():
+        model = create_model2()
+        model.set_weights( weights_param.args )
+        model.save( filename )
 
-def score_similation( weights ):
-    model = create_model()
-    model.set_weights([tf.convert_to_tensor(arg, dtype=tf.float32) for arg in weights.args])
+def score_simulation( *weights ):
+    gra = tf.Graph()
+    with gra.as_default():
+        model = create_model2()
+        model.set_weights( weights )
 
-    scores = np.zeros( 10 )
-    #play 10 games
-    for i in range( 0, 10 ):
-        scores[ i ] = run_single_simulation( model )
-        #perform transform
-        if scores[ i ] != 0:
-            scores[ i ] = -1.0 * math.log10( scores[ i ] )
-            
-    #return mean
-    return np.mean( scores )
-    
-'''
-#vars = [ng.var.Array(*tuple(weight.shape)).bounded(-10, 10) for weight in model.trainable_weights]
+        scores = np.zeros( 10 )
+        #play 10 games
+        for i in range( 0, 10 ):
+            scores[ i ] = run_single_simulation( model )
+            #perform transform
+            if scores[ i ] != 0:
+                scores[ i ] = -1.0 * math.log10( scores[ i ] )
 
-#OnePlusOne works pretty well
-#TBPSA bugs out on me
-#MEDA slows down pretty quickly
-#RealSpacePSO seems to do a pretty bad job
-#NaiveTBPSA doesn't bug out but isn't great
-tool = "OnePlusOne"
-budget=1000
-optimizer = ng.optimizers.registry[tool](parametrization=ng.p.Instrumentation(*vars), budget=budget )
-
-dummy_input1 = np.zeros( shape=(1,in1_dim,in1_dim,n_board_states) )
-dummy_input2 = np.zeros( shape=(1,3,3,n_board_states) )
-
-#https://github.com/bionicles/neuromax/blob/53385b2566503dc7613a6c7a95d37e747c61887b/archive/scripts/nevergrad-interface.py
-initial_weights = model.get_weights()
-initial_weights = [np.random.permutation(w.flat).reshape(w.shape) for w in initial_weights]
-# Faster, but less random: only permutes along the first dimension
-# weights = [np.random.permutation(w) for w in weights]
-model.set_weights(initial_weights)
-
-for _ in range( 0, 1000 ):
-        weights = optimizer.ask()
-        model.set_weights([tf.convert_to_tensor(arg, dtype=tf.float32) for arg in weights.args])
-        dummy_preds = model.predict( [dummy_input1,dummy_input2] )
-        print( dummy_preds )
-        #score = dummy_preds[0][0]
-        #score = np.sqrt(np.mean(dummy_preds**2))
-        #score = np.amin( dummy_preds ) - np.amax( dummy_preds )
-        score = 0
-        for x in dummy_preds[0]:
-            score += abs( x - 0.1 )
-        #score = abs( dummy_preds[0][1] - dummy_preds[0][2] )
-        optimizer.tell(weights, score)
-        #exit( 0 )
-
-print( "Final:" )
-        
-final_weights = optimizer.provide_recommendation()
-model.set_weights([tf.convert_to_tensor(arg, dtype=tf.float32) for arg in final_weights.args])
-dummy_preds = model.predict( [dummy_input1,dummy_input2] )
-print( dummy_preds )
-        
-#while true
-'''
+        #return mean
+        return np.mean( scores )

5f2762c34b848af8300d6b082f1e5fc867e0561a
JackMaguire <jackmaguire1444@gmail.com>
2020-08-01 14:00:24 +0000

    running best is always output

--- nevergrad_training/run_worker.py
+++ nevergrad_training/run_worker.py
@@ -5,6 +5,7 @@ from robots import *
 def run_worker( comm, rank, out_prefix ):
 
     i = 0
+    best_score = -1.0
     
     while True:
         i += 1
@@ -17,8 +18,9 @@ def run_worker( comm, rank, out_prefix ):
             comm.send( 0, dest=0, tag=0 )
             break
 
-        potential_filename = str( comm ) + "_" + str(i) + ".h5"
-        final_score = score_similation( dofs, potential_filename )
-
+        potential_filename = str( comm ) + "_" + str(i)
+        final_score = score_similation( dofs, potential_filename, best_score )
+        best_score = min( best_score, final_score )
+        
         bundle = [ dofs, final_score, potential_filename ]
         comm.send( bundle, dest=0, tag=1 )

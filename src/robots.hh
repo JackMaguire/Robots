1cc7bcfe42dbc4b9a51507e0d5b6a7224700b90d
Jack Maguire <jackmaguire1444@gmail.com>
2021-01-01 10:18:18 -0500

    forecasting

--- src/robots.hh
+++ src/robots.hh
@@ -415,6 +415,53 @@ private:
 };
 }
 
+struct ForecastResults {
+  bool legal = false;
+  bool cascade_safe = false;
+  unsigned int robots_killed = 0;
+};
+
+ForecastResults
+forcast_move( Board const & board, int const dx, int const dy ){
+  ForecastResults results;
+  Board copy = board;
+
+  MoveResult move_result = copy.move_human( dx, dy );
+  if( move_result == MoveResult::YOU_LOSE ) return results;  
+
+  results.legal = true;
+  results.robots_killed = board.n_robots() - copy.n_robots();
+
+  //Check for cascade safety
+  while ( move_result == MoveResult::CONTINUE ){
+    move_result = copy.move_human( 0, 0 );
+  }
+  results.cascade_safe = (move_result != MoveResult::YOU_LOSE);
+
+  return results;
+}
+
+__attribute__((unused))
+std::array< std::array< ForecastResults, 3 >, 3 >
+forcast_all_moves( Board const & board ) {
+  std::array< std::array< ForecastResults, 3 >, 3 > forecasts;
+
+  Position const human_position = board.human_position();
+
+  for( int dx = -1; dx <= 1; ++dx ){
+    if( human_position.x+dx < 0 || human_position.x+dx >= WIDTH ) continue;
+
+    for( int dy = -1; dy <= 1; ++dy ){
+      if( human_position.y+dy < 0 || human_position.y+dy >= HEIGHT ) continue;
+
+      forecasts[ dx+1 ][ dy+1 ] = forcast_move( board, dx, dy );
+    }
+  }
+
+  return forecasts;
+}
+
+
 struct NullVisualizer {
   static void show( Board const & ){}
 };
